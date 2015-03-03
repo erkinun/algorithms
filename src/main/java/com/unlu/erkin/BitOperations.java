e6f7d5dd96224e52d95f35b9f11dc1d01b3c2dd7
lost_mohican <erkinun@gmail.com>
2015-03-03 21:50:41 +0200

    bit count diffs

--- src/main/java/com/unlu/erkin/BitOperations.java
+++ src/main/java/com/unlu/erkin/BitOperations.java
@@ -7,6 +7,16 @@ public class BitOperations {
 
     public static void main(String[] args) {
         bitSubstring(0x0015,0x0400, 2, 6);
+
+        int hede = 53;
+
+        while (hede > 0) {
+            hede = hede >> 1;
+        }
+
+        System.out.println("hede is now: " + hede);
+
+        System.out.println("we have to change for 31 and 14: " + setCountDiff(31, 14));
     }
 
     public static void bitSubstring(int n, int m, int start, int j) {
@@ -27,5 +37,30 @@ public class BitOperations {
         int result = m | temp;
 
         System.out.println(Integer.toBinaryString(result));
+    }
+
+    public static int setCountDiff(int a, int b) {
+        int a1 = 0;
+        int b1 = 0;
+
+        int temp = a;
+
+        while (temp > 0) {
+            if ((temp & 0x01) == 1) {
+                a1++;
+            }
+            temp = temp >> 1;
+        }
+
+        temp = b;
+
+        while (temp > 0) {
+            if ((temp & 0x01) == 1) {
+                b1++;
+            }
+            temp = temp >> 1;
+        }
+
+        return Math.abs(a1 - b1);
     }
 }
