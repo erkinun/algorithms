c13db9cfecafb2b3bb7a264f5ddde72c843fb696
lost_mohican <erkinun@gmail.com>
2015-03-09 22:43:21 +0200

    a generic quicksort

--- src/main/java/com/unlu/erkin/QuickSortImpl.java
+++ src/main/java/com/unlu/erkin/QuickSortImpl.java
@@ -8,39 +8,39 @@ import java.util.Arrays;
 public class QuickSortImpl {
 
     public static void main(String[] args) {
-        int[] data = {5,1,4,8,9,2,11};
+        Integer[] data = {5,1,4,8,9,2,11};
 
         quickSort(data, 0, data.length - 1);
 
         System.out.println(Arrays.toString(data));
     }
 
-    private static void quickSort(int[] data, int lo, int hi) {
+    private static <T extends Comparable<T>> void quickSort(T[] data, int lo, int hi) {
         if (lo >= hi) {
             return;
         }
 
-        int pivot = data[lo];
+        T pivot = data[lo];
 
         int curLo = lo + 1;
         int curHi = hi;
 
         while (true) {
-            while (data[curLo] < pivot) { curLo++; }
-            while (data[curHi] > pivot && curHi != 0) { curHi--; }
+            while (data[curLo].compareTo(pivot) < 0) { curLo++; }
+            while (data[curHi].compareTo(pivot) > 0 && curHi != 0) { curHi--; }
 
             if (curHi == 0) { return; }
 
             if (curHi <= curLo) {
                 //swap pivot
                 int pivotInd;
-                if (pivot < data[curLo]) {
+                if (pivot.compareTo(data[curLo]) < 0) {
                     pivotInd = curLo - 1;
                 }
                 else {
                     pivotInd = curLo;
                 }
-                int temp = data[pivotInd];
+                T temp = data[pivotInd];
                 data[pivotInd] = pivot;
                 data[lo] = temp;
                 //quicksort left
@@ -51,7 +51,7 @@ public class QuickSortImpl {
                 return;
             }
 
-            int temp = data[curLo];
+            T temp = data[curLo];
             data[curLo] = data[curHi];
             data[curHi] = temp;
             curLo++;
