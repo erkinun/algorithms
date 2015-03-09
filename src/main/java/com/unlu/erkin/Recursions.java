b2ccf9f1c122cf1e3ffc0a02efc06abae7a8ba71
Erkin Unlu <erkin.unlu@monitise.com>
2015-03-09 16:50:03 +0200

    Merge branch 'master' of https://github.com/lostMohican/algorithms
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/com/unlu/erkin/Recursions.java
+++ src/main/java/com/unlu/erkin/Recursions.java
@@ -1,5 +1,8 @@
 package com.unlu.erkin;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by ERKIN on 29/01/15.
  */
@@ -12,6 +15,16 @@ public class Recursions {
         binarySearch(sorted, 1);
         binarySearch(sorted, 4);
         binarySearch(sorted, 7);
+
+        perms("", "hat");
+
+        Set<String> combsEmpty = new HashSet<>();
+        combsEmpty.add("");
+        Set<String> combs = combs("wxyz", 0, combsEmpty);
+
+        combs.stream()
+                .sorted()
+                .forEach(comb -> System.out.println(comb));
     }
 
     private static void binarySearch(int[] sorted, int num) {
@@ -38,5 +51,35 @@ public class Recursions {
         else {
             innerBinary(sorted, num, index+1, end);
         }
+    }
+
+    private static void perms(String perm, String word) {
+        if (word == null || word.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+
+        int i = 0;
+        for (Character c : word.toCharArray()) {
+            StringBuilder builder = new StringBuilder(word);
+            perms(perm + c, builder.deleteCharAt(i).toString());
+            i++;
+        }
+    }
+
+    private static Set<String> combs(String word, int index, Set<String> combs) {
+        if (index >= word.length()) {
+            return combs;
+        }
+
+        char c = word.charAt(index);
+
+        Set<String> newSet = new HashSet<>();
+        for (String comb : combs) {
+            newSet.add(comb + c);
+            newSet.add(comb);
+        }
+
+        return combs (word, index+1, newSet);
     }
 }
