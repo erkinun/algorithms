2e3a528b3db322b0a04b6cbc41b5e8c46b8b24f3
lost_mohican <erkinun@gmail.com>
2015-04-19 19:19:39 +0300

    find palindromes of string

--- src/main/java/com/unlu/erkin/Words.java
+++ src/main/java/com/unlu/erkin/Words.java
@@ -1,5 +1,8 @@
 package com.unlu.erkin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by unlue on 06/03/15.
  */
@@ -10,6 +13,12 @@ public class Words {
         System.out.println(nextPalindrome("1392"));
 
         System.out.println(removeDuplicates("tree traversal"));
+
+        List<String> substrs = palindromes("helle");
+
+        for (String s : substrs) {
+            System.out.println(s);
+        }
     }
 
     private static int nextPalindrome(String number) {
@@ -82,5 +91,54 @@ public class Words {
 
 
         return new String(stringA).substring(0, tail);
+    }
+
+    private static List<String> findSubstr(String word) {
+        List<String> all = new ArrayList<>();
+        List<String> last = new ArrayList<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            last.add(word.substring(0, i+1));
+        }
+
+        all.addAll(last);
+
+        while (last.size() > 0) {
+            List<String> newSet = new ArrayList<>();
+
+            for (String s : last) {
+                if (s.length() > 1) {
+                    newSet.add(s.substring(1, s.length()));
+                }
+            }
+
+            all.addAll(newSet);
+            last = newSet;
+        }
+
+        return all;
+    }
+
+    private static List<String> palindromes(String word) {
+        List<String> substrs = findSubstr(word);
+
+        List<String> pals = new ArrayList<>();
+        for (String s : substrs) {
+            if (isPalindrome(s)) {
+                pals.add(s);
+            }
+        }
+
+        return pals;
+    }
+
+    private static boolean isPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
